5a3f812709ae18cccd6c77157ed464942294456f
pedrodel26 <pedromodesto26@gmail.com>
2024-08-22 14:32:25 -0300

    Initial Commit

---
+++ CarApp/CarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAppApp.swift
+//  CarApp
+//
+//  Created by Pedro Delmondes  on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
